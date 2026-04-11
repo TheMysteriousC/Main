ad96ad51f85d083cbb9295e60a8d1279642f1c61
TheMysteriousC <cookielord32@gmail.com>
2026-04-11 01:12:54 +0100

    Update 14.1 CookieWare.lua

--- CookieWare.lua
+++ CookieWare.lua
@@ -979,7 +979,7 @@ Subsections.DropItems:Toggle{
 
 Subsections.DropSettings:Slider{
     Name = "Loop Interval (seconds)",
-    Min = 0.00000001,
+    Min = 0.00000000000000001,
     Max = 10,
     Default = Config.Drop.LoopInterval,
     Flag = "LoopDropInterval",
